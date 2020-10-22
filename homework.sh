617c3a363df7220fa207ab44816b109b3d8e048a
shunak <shuna966@gmail.com>
2020-10-22 19:19:15 +0900

    modified

--- homework.sh
+++ homework.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ $# != 2 ]; then
-    echo "Error: Need your GitHub Username and Repository's visibility, input Username as first argument and Repos visibility public or private as second argument." $*
+    echo "Error: Need your GitHub Username and Repository's visibility, input Username as first argument and Repository's visibility public or private as second argument." $*
     exit 1
 else
     echo "Start creating New Repository on your GitHub..."
