a55b6761c0773597cd716b2f123537791c25808e
shunak <shuna966@gmail.com>
2021-08-11 15:21:41 +0900

    Fixed to fit to 2FactorAuthentication

--- wfh.sh
+++ wfh.sh
@@ -26,7 +26,7 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/$1/${reposname}.git
+git remote add origin git@github.com:$1/${reposname}.git
 git push -u origin main
 
 
